752bf5901aca589ef4812878828d22509a895cd8
vinhthanh89 <vinhthanh89@gmail.com>
2023-12-18 21:48:30 +0700

    react lesson 4

---
+++ src/components/profile.js
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <div className ="profile" style = {{ backgroundColor : "black" , padding : "20px 50px"}}>
+      <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+      <h4>image name</h4>
+    </div>
+  );
+};
+
+export default Profile;
